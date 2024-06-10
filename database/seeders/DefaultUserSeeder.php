39e8e52d77c960e792af1eadae527da4f86c810d
Arman <2001armanmaulana@gmail.com>
2024-06-10 19:03:26 +0700

    fix user

--- database/seeders/DefaultUserSeeder.php
+++ database/seeders/DefaultUserSeeder.php
@@ -24,46 +24,11 @@ class DefaultUserSeeder extends Seeder
                 'contact' => '1234567890',
                 'gender' => User::MALE,
                 'type' => User::ADMIN,
-                'email' => 'admin@unmul.com',
+                'email' => 'admin@polnes.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => Hash::make('123456'),
                 'region_code' => '91',
             ],
-            [
-                'first_name' => 'Adam',
-                'last_name' => 'Diaz',
-                'contact' => '1234567890',
-                'gender' => User::MALE,
-                'type' => User::DOCTOR,
-                'email' => 'doctor@infycare.com',
-                'email_verified_at' => Carbon::now(),
-                'password' => Hash::make('123456'),
-            ],
-            [
-                'first_name' => 'Aiko',
-                'last_name' => 'Walsh',
-                'contact' => '1234567890',
-                'gender' => User::MALE,
-                'type' => User::PATIENT,
-                'email' => 'patient@infycare.com',
-                'email_verified_at' => Carbon::now(),
-                'password' => Hash::make('123456'),
-            ],
         ];
-
-        foreach ($users as $key => $user) {
-            $user = User::create($user);
-            if ($key == 1) {
-                $doctor = Doctor::create(['user_id' => $user->id]);
-                $user->address()->create(['owner_id' => $user->id]);
-            }
-            if ($key == 2) {
-                $patient = Patient::create(['user_id' => $user->id, 'patient_unique_id' => 'UNIQUE12']);
-                $patient->address()->create(['owner_id' => $patient['user_id']]);
-            }
-        }
-
-        $specializationIds = Specialization::pluck('id');
-        $doctor->specializations()->sync($specializationIds);
     }
 }
